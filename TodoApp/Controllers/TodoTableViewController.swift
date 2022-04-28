be3bd2af1c40215fcf4adb0770441f9a4a161602
Aakriti <aakriti.nautiyal@unthinkable.co>
2022-04-28 09:36:38 +0530

    Implementing delete items

--- TodoApp/Controllers/TodoTableViewController.swift
+++ TodoApp/Controllers/TodoTableViewController.swift
@@ -18,6 +18,7 @@ class TodoTableViewController: UITableViewController {
         loadItems()
     }
     
+    //Create
     @IBAction func addNewItem(_ sender: Any) {
         var itemTextField = UITextField()
         let alertController = UIAlertController(title: "Add new item", message: nil, preferredStyle: .alert)
@@ -38,6 +39,7 @@ class TodoTableViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    //Read
     func loadItems(){
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         do{
@@ -62,7 +64,6 @@ class TodoTableViewController: UITableViewController {
         return items.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         let item = items[indexPath.row]
@@ -71,31 +72,25 @@ class TodoTableViewController: UITableViewController {
         return cell
     }
     
+    //Update
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let item = items[indexPath.row]
         item.isChecked = !item.isChecked
         self.saveItems()
     }
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
+    //Delete
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let itemToBeDeleted = items[indexPath.row]
+            context.delete(itemToBeDeleted)
+            items.remove(at: indexPath.row)
+            saveItems()
+        }
     }
-    */
-
-
 }
